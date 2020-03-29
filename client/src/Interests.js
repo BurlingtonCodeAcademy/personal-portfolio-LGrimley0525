6900331edff1b4c590876b1ce3806ffd4d30a800
LGrimley0525 <60547230+LGrimley0525@users.noreply.github.com>
2020-03-28 23:36:10 -0400

    to upgrade portfolio and interests

--- client/src/Interests.js
+++ client/src/Interests.js
@@ -1,45 +1,119 @@
 import React from 'react'
+import ReactAnimation from 'react-animation'
+import { AnimateOnChange, HideUntilLoaded } from 'react-animation'
 import Tobeyphoto from './hobbyphotos/Tobey.png'
+import EthanAllen from './hobbyphotos/EthanAllen.png'
 import Burlingtonphoto from './hobbyphotos/Burlington.png'
 import Billsphoto from './hobbyphotos/BillsStadium.png'
 import Burpeephoto from './hobbyphotos/Burpee.png'
 import Familyphoto from './hobbyphotos/Family.png'
+import Wings from './hobbyphotos/Wings.png'
+import Quebec from './hobbyphotos/Quebec.png'
+import InterestHeaderPic from './hobbyphotos/InterestHeaderPic.png'
+import Christmas from './hobbyphotos/Christmas.png'
+
+const { useState, useLayoutEffect, useRef } = React
+
 
 
 
 function Interests(props) {
+    const words = [
+        'Burlingtonian.',
+        'Dill Pickles.',
+        'Strong.',
+        'Cocktails on the Porch.',
+        'Buffalo Bills.',
+        'My Husband, Nick.',
+        'Coffee & the Morning News.',
+        'Fighting for What is Right.',
+        'Red Sangria.',
+        'My Family.',
+        'Problem-Solver.',
+        'My Corgi-mix, Tobey.',
+        'Hallmark Christmas movies.',
+        'Morning Workouts.',
+        'Compassionate.',
+        'Mexican Food.',
+        'Motivator.',
+    ]
+    const [current, setCurrent] = useState(0)
+    const [currentWidth, setCurrentWidth] = useState(0)
+    const [currentTextStyle, setCurrentTextStyle] = useState({})
+    const currentTextRef = useRef()
+    const nextTextRef = useRef()
+
+    const nextItem = current => {
+        if (current === words.length - 1) {
+            return 0
+        } else {
+            return current + 1
+        }
+    }
+    useLayoutEffect(() => {
+        const interval = setInterval(() => {
+            setCurrent(nextItem(current))
+            nextTextRef.current.innerText = words[nextItem(current)]
+            const nextTextSize = nextTextRef.current.offsetWidth
+            setCurrentWidth(nextTextRef.current.offsetWidth)
+            setCurrentTextStyle({
+                opacity: 0
+            })
+            setTimeout(() => {
+                setCurrentTextStyle({
+                    opacity: 1
+                })
+            }, 500)
+        }, 2000);
+        return (() => {
+            clearInterval(interval)
+        })
+    })
+
+
+
     return (
+
         <div>
-            <div className='interests'>
-                <div id='interest-title'>
-                    <h1> When I'm not at my computer...</h1>
-                </div>
-                <div id='burlington-pic'>
-                    <img src={Burlingtonphoto} alt="Burlingtonphoto"></img>
-                </div>
-                <div id='top-info'> 
-                    <h2>I'm just a Buffalo girl living her best life in Vermont. </h2>
-                </div>
+            <div id='burl-pic'>
+                <img src={EthanAllen} alt='burlington'></img>
+            </div>
+            <div id='interest-text'>Who I Am & What I Love: <br /> <br />
+                <AnimateOnChange className="foo" durationOut={500}>
+                    <div className="contain" style={{ width: currentWidth ? currentWidth + 'px' : 'auto' }}>
+                        <div className="text-width-wrapper" style={currentTextStyle} ref={currentTextRef}>{words[current]}
+                        </div>
+                    </div>
+                </AnimateOnChange>
+            </div>
+            <h1 className="hidden-text" ref={nextTextRef} />
+
+            <div className='picturesection'>
                 <div id='tobey-pic'>
-                    <img src={Tobeyphoto} alt="Tobeyphoto"></img>
-                </div>
-                <div id='interest-text2'>
-                    <p> I live for Sunday Football & the Buffalo Bills, actually all things Buffalo! I enjoy exercising first thing in the morning, memorizing all the lyrics to my favorite songs, and spending time with my husband, Nick, and my rescue-pup, Tobey! </p>
-                    <p>After moving to Vermont 7 years ago, I have discovered the beauty in a more rural setting, and the appreciation of a good pair of winter gloves!</p>
-                </div>
-                <div id='burpee-pic'>
-                    <img src={Burpeephoto} alt="Burpeephoto"></img>
+                    <img src={Tobeyphoto} alt='tobey'></img>
                 </div>
                 <div id='bills-pic'>
-                    <img src={Billsphoto} alt="Billsphoto"></img>
+                    <img src={Billsphoto} alt='bills'></img>
                 </div>
                 <div id='family-pic'>
-                    <img src={Familyphoto} alt="Familyphoto"></img>
+                    <img src={Familyphoto} alt='family'></img>
                 </div>
+                <div id='burpee-pic'>
+                    <img src={Burpeephoto} alt='burpee'></img>
+                </div>
+                <div id='wings'>
+                    <img src={Wings} alt='wings'></img>
+                </div>
+                <div id='quebec-pic'>
+                    <img src={Quebec} alt='quebec'></img>
+                </div>
+               
+
             </div>
-        </div>
+        </div>    
     )
 }
+
 
 
 export default Interests
--- client/src/Portfolio.js
+++ client/src/Portfolio.js
@@ -9,42 +9,62 @@ import './App.css'
 function Portfolio(props) {
     return (
         <div className='portfolio'>
-            <div id='title'> 
+            <div id='title'>
                 <h1>My Work</h1>
-                <h2>Here are a few projects I have worked on recently.</h2> 
+                <h2>Here are a few projects I have worked on recently.</h2>
             </div>
             <div id='geovermont'>
                 <h3> Geo-Vermonter </h3>
                 <img src={GeoVermont} alt="GeoVermont"></img>
-                <h4> A Vermont geographic county guessing game using React and Leaflet.js library. <br /> Click <a href='https://github.com/BurlingtonCodeAcademy/geo-vermonter-leepeters0023'>here</a> for the code. 
-                </h4>
+                <div id='fadedbox'>
+                    <div id='text'>
+                        <h4> A Vermont geographic county guessing game using React and Leaflet.js library.<br /> <br /> Click <a href='https://github.com/BurlingtonCodeAcademy/geo-vermonter-leepeters0023'>here</a> for the code.
+                        </h4>
+                    </div>
+                </div>
             </div>
             <div id='yelp'>
                 <h3> Yelpington </h3>
                 <img src={Yelpington} alt="Yelpington"></img>
-                <h4>A directory of Burlington restaurants that uses AJAX to retrieve JSON data and Leaflet. <br />  Click <a href='https://github.com/BurlingtonCodeAcademy/yelpington-LGrimley0525'>here</a> for the code.
-                </h4>
+                <div id='fadedbox'>
+                    <div id='text'>
+                        <h4>A directory of Burlington restaurants that uses AJAX to retrieve JSON data and Leaflet.<br /> <br />  Click <a href='https://github.com/BurlingtonCodeAcademy/yelpington-LGrimley0525'>here</a> for the code.
+                        </h4>
+                    </div>
+                </div>
             </div>
             <div id='remock'>
                 <h3> Website Remock </h3>
                 <img src={Remock} alt="Remock"></img>
-                <h4> Re-creation of a complicated blog-style webpage using CSS and HTML. <br /> Click <a href='https://github.com/BurlingtonCodeAcademy/remock-bapzen'>here</a> for the code.
-                </h4>
+                <div id='fadedbox'>
+                    <div id='text'>
+                        <h4> Re-creation of a complicated blog-style webpage using CSS and HTML.<br /> <br /> Click <a href='https://github.com/BurlingtonCodeAcademy/remock-bapzen'>here</a> for the code.
+                        </h4>
+                    </div>
+                </div>
             </div>
             <div id='tictactoe'>
                 <h3> Tic-Tac-Toe </h3>
                 <img src={TicTacToe} alt="TicTacToe"></img>
-                <h4> Classic game using JS, CSS and HTML that user can play another user or against computer. <br /> Click <a href='https://github.com/BurlingtonCodeAcademy/tic-tac-toe-LGrimley0525'>here</a> for the code. 
-                </h4>
+                <div id='fadedbox'>
+                    <div id='text'>
+                        <h4> Classic game using JS, CSS and HTML that user can play another user or against computer. <br /> <br /> Click <a href='https://github.com/BurlingtonCodeAcademy/tic-tac-toe-LGrimley0525'>here</a> for the code.
+                        </h4>
+                    </div> 
+                </div>   
             </div>
             <div id='zork'>
                 <h3> Zorkington </h3>
                 <img src={Zorkington} alt="Zorkington"></img>
-                <h4> A text adventure game in JavaScript. <br /> Click <a href='https://github.com/BurlingtonCodeAcademy/zorkington-vermontdaniel-1'>here</a> for the code.
-                </h4>
+                <div id='zorkfadedbox'>
+                    <div id='zork-text'>
+                        <h4> A text adventure game in JavaScript. <br /> <br /> Click <a href='https://github.com/BurlingtonCodeAcademy/zorkington-vermontdaniel-1'>here</a> for the code.
+                        </h4>
+                    </div>
+                </div>    
             </div>
         </div>
-)
+    )
 }
 
 export default Portfolio
